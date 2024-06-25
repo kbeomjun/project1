9d06c278979c4bddf6b497326c2ea4a387196821
kbeomjun <98301971+kbeomjun@users.noreply.github.com>
2024-06-26 08:43:37 +0900

    ATM

--- project1/src/atm/ATMManger.java
+++ project1/src/atm/ATMManger.java
@@ -113,11 +113,11 @@ public class ATMManger {
 	}
 	
 	private String createAccountNum() {
-		int min = 1, max = 9999;
+		int min = 1, max = 999;
 		int random = (int)(Math.random() * (max - min + 1) + min);
 		String randomNum = String.valueOf(random);
 		if(randomNum.length() < 4) {
-			while(randomNum.length() == 4) {
+			while(randomNum.length() < 4) {
 				randomNum = "0" + randomNum;
 			}
 		}

c2233e68debf2e86cb58db18c3f368f229da35f7
kbeomjun <98301971+kbeomjun@users.noreply.github.com>
2024-06-28 12:47:18 +0900

    ATM2

--- project1/src/atm2/ATM.java
+++ project1/src/atm2/ATM.java
@@ -12,31 +12,20 @@ import java.util.regex.Pattern;
 
 public class ATM {
 	private Scanner scan = new Scanner(System.in);
-	private List<Account> list = new ArrayList<Account>();
 	private String ip = "192.168.30.22";
 	private int port = 5001;
 	
 	private void printMenu() {
-		System.out.print("======ATM======\n"
-				+ "1. 계좌개설\n"
-				+ "2. 계좌관리\n"
-				+ "3. 업무종료\n"
-				+ "선택 : ");
-	}
-	private void printMenu2() {
-		System.out.print("======계좌관리======\n"
-				+ "1. 비밀번호 변경\n"
-				+ "2. 계좌해지\n"
-				+ "3. 입금/출금/송금\n"
-				+ "4. 통장조회\n"
-				+ "5. 이전으로\n"
-				+ "선택 : ");
+		System.out.print(
+				"==================ATM==================\n"
+				+ "1. 계좌개설   2. 계좌해지   3. 비밀번호 변경\n"
+				+ "4. 입금      5. 출금     6. 송금\n"
+				+ "7. 통장조회   8. 업무종료   선택 : ");
 	}
 	
 	private void runMenu(int menu) {
-		
 		System.out.println("------------------------------");
-		A:switch(menu) {
+		switch(menu) {
 		case 1:
 			try {
 				Socket socket = new Socket(ip, port);
@@ -69,205 +58,349 @@ public class ATM {
 			}
 			break;
 		case 2:
-			int index = findAccount();
-			if(index == -1) {
-				break;
-			}
-			checkPassWord(index);
-			int select = 0;
-			do {
-				printMenu2();
-				select = scan.nextInt();
+			try {
+				Socket socket = new Socket(ip, port);
+				ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+				ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+				oos.writeUTF("remove");
+				oos.flush();
+				System.out.print("계좌번호 혹은 예금주명 : ");
+				String search = scan.next();
+				oos.writeUTF(search);
+				oos.flush();
+				int index = ois.readInt();
+				if(index == -1) {
+					System.out.println("없는 계좌입니다.");
+					break;
+				}
+				String check = "";
+				do {
+					System.out.print("비밀번호 : ");
+					String password = scan.next();
+					oos.writeUTF(password);
+					oos.flush();
+					check = ois.readUTF();
+					if(check.equals("잘못된 비밀번호입니다. 다시 입력하세요.")) {
+						System.out.println(check);
+						continue;
+					}
+					break;
+				}while(true);
+				System.out.println("계좌를 해지했습니다.");
 				System.out.println("------------------------------");
-				switch(select) {
-				case 1:
-					String password = "";
-					do {
-						System.out.print("변경할 비밀번호(4자리) : ");
-						password = scan.next();
-						String regex = "^\\d{4}$";
-						if(!Pattern.matches(regex, password)) {
-							System.out.println("잘못된 비밀번호 형식입니다. 다시 입력하세요.");
-							continue;
-						}
-						break;
-					}while(true);
-					list.get(index).setPassword(password);
-					System.out.println("비밀번호를 변경했습니다.");
-					System.out.println("------------------------------");
-					break A;
-				case 2:
-					list.remove(index);
-					System.out.println("계좌를 해지했습니다.");
-					System.out.println("------------------------------");
-					break A;
-				case 3:
-					System.out.print("1. 입금\n"
-							+"2. 출금\n"
-							+"3. 송금\n"
-							+"선택 : ");
-					select = scan.nextInt();
-					System.out.println("------------------------------");
-					switch(select) {
-					case 1:
-						System.out.print("입금할 금액 : ");
-						int deposit = scan.nextInt();
-						if(deposit <= 0) {
-							System.out.println(deposit+"원은 입금할 수 없습니다.");
-							break;
-						}
-						int money = list.get(index).getBalance() + deposit;
-						list.get(index).setBalance(money);
-						System.out.println(deposit+"원을 입금하였습니다.");
-						System.out.println(list.get(index).getName()+"님의 남은 잔고 : "+list.get(index).getBalance()+"원");
-						list.get(index).getBankBook().add("입금 "+deposit+"원 | 잔고 : "+list.get(index).getBalance()+"원");
-						System.out.println("------------------------------");
-						break;
-					case 2:
-						if(list.get(index).getBalance() == 0) {
-							System.out.println("잔액이 0원이므로 출금할 수 없습니다.");
-							break;
-						}
-						int withdraw = 0;
-						do {
-							System.out.print("출금할 금액(잔액 : "+list.get(index).getBalance()+"원) : ");
-							withdraw = scan.nextInt();
-							if(withdraw <= 0 || list.get(index).getBalance() < withdraw) {
-								System.out.println(withdraw+"원은 출금할 수 없습니다.");
-								continue;
-							}
-							break;
-						}while(true);
-						money = list.get(index).getBalance() - withdraw;
-						list.get(index).setBalance(money);
-						System.out.println(withdraw+"원을 출금하였습니다.");
-						System.out.println(list.get(index).getName()+"님의 남은 잔고 : "+list.get(index).getBalance()+"원");
-						list.get(index).getBankBook().add("출금 "+withdraw+"원 | 잔고 : "+list.get(index).getBalance()+"원");
-						System.out.println("------------------------------");
-						break;
-					case 3:
-						if(list.get(index).getBalance() == 0) {
-							System.out.println("잔액이 0원이므로 송금할 수 없습니다.");
-							break;
-						}
-						System.out.print("송금할 계좌번호 혹은 예금주명 : ");
-						String search = scan.next();
-						List<Account> list2 = new ArrayList<Account>();
-						for(int i = 0; i < list.size(); i++) {
-							if(index == i) {
-								continue;
-							}
-							if(list.get(i).getAccountNum().contains(search) || list.get(i).getName().contains(search)) {
-								list2.add(list.get(i));
-							}
-						}
-						if(list2.size() == 0) {
-							System.out.println("송금할 계좌가 없습니다.");
-							break;
-						}
-						for(int i = 0; i < list2.size(); i++) {
-							System.out.println((i+1)+". "+list2.get(i).getBank()+" "+list2.get(i).getAccountNum()+"(예금주:"+list2.get(i).getName()+")");
-						}
-						System.out.print("송금할 계좌 선택 : ");
-						int index2 = scan.nextInt() - 1;
-						int transfer = 0;
-						do {
-							System.out.print("송금할 금액(잔액 : "+list.get(index).getBalance()+"원) : ");
-							transfer = scan.nextInt();
-							if(transfer <= 0 || list.get(index).getBalance() < transfer) {
-								System.out.println(transfer+"원은 송금할 수 없습니다.");
-								continue;
-							}
-							break;
-						}while(true);
-						money = list.get(index).getBalance() - transfer;
-						list.get(index).setBalance(money);
-						System.out.println(transfer+"원을 송금하였습니다.");
-						System.out.println(list.get(index).getName()+"님의 남은 잔고 : "+list.get(index).getBalance()+"원");
-						list.get(index).getBankBook().add("송금 "+transfer+"원 | 잔고 : "+list.get(index).getBalance()+"원");
-						for(int i = 0; i < list.size(); i++) {
-							if(list.get(i).getAccountNum().equals(list2.get(index2).getAccountNum())) {
-								index2 = i;
-							}
-						}
-						money = list.get(index2).getBalance() + transfer;
-						list.get(index2).setBalance(money);
-						list.get(index2).getBankBook().add(list.get(index).getName()+" "+transfer+"원 | 잔고 : "+list.get(index2).getBalance()+"원");
-						System.out.println("------------------------------");
-					default:
-						System.out.println("잘못된 메뉴입니다.");
-						System.out.println("------------------------------");
+				break;
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		case 3:
+			try {
+				Socket socket = new Socket(ip, port);
+				ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+				ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+				oos.writeUTF("update");
+				oos.flush();
+				System.out.print("계좌번호 혹은 예금주명 : ");
+				String search = scan.next();
+				oos.writeUTF(search);
+				oos.flush();
+				int index = ois.readInt();
+				if(index == -1) {
+					System.out.println("없는 계좌입니다.");
+					break;
+				}
+				String check = "";
+				do {
+					System.out.print("비밀번호 : ");
+					String password = scan.next();
+					oos.writeUTF(password);
+					oos.flush();
+					check = ois.readUTF();
+					if(check.equals("잘못된 비밀번호입니다. 다시 입력하세요.")) {
+						System.out.println(check);
+						continue;
 					}
 					break;
-				case 4:
-					System.out.println(list.get(index).toString());
-					System.out.println("============통장내역============");
-					List<String> bankBook = list.get(index).getBankBook();
-					for(int i = 0; i < bankBook.size(); i++) {
-						System.out.println((i+1)+". "+bankBook.get(i));
+				}while(true);
+				String password = "";
+				do {
+					System.out.print("변경할 비밀번호(4자리) : ");
+					password = scan.next();
+					String regex = "^\\d{4}$";
+					if(!Pattern.matches(regex, password)) {
+						System.out.println("잘못된 비밀번호 형식입니다. 다시 입력하세요.");
+						continue;
 					}
-					System.out.println("==============================");
-					System.out.print("돌아가려면 엔터를 입력하세요.");
-					scan.nextLine();
-					String enter = scan.nextLine();
-					if(enter.equals("\n")) {
-						System.out.println("------------------------------");
+					break;
+				}while(true);
+				oos.writeUTF(password);
+				oos.flush();
+				System.out.println("비밀번호를 변경했습니다.");
+				System.out.println("------------------------------");
+				break;
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		case 4:
+			try {
+				Socket socket = new Socket(ip, port);
+				ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+				ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+				oos.writeUTF("deposit");
+				oos.flush();
+				System.out.print("입금할 금액 : ");
+				int deposit = scan.nextInt();
+				oos.writeInt(deposit);
+				oos.flush();
+				String result = ois.readUTF();
+				if(result.equals(deposit+"원은 입금할 수 없습니다.")) {
+					System.out.println(result);
+					break;
+				}
+				System.out.print("계좌번호 혹은 예금주명 : ");
+				String search = scan.next();
+				oos.writeUTF(search);
+				oos.flush();
+				int index = ois.readInt();
+				if(index == -1) {
+					System.out.println("없는 계좌입니다.");
+					break;
+				}
+				System.out.println(deposit+"원을 입금하였습니다.");
+				result = ois.readUTF();
+				System.out.println(result);
+				System.out.println("------------------------------");
+				break;
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		case 5:
+			try {
+				Socket socket = new Socket(ip, port);
+				ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+				ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+				oos.writeUTF("withdraw");
+				oos.flush();
+				System.out.print("계좌번호 혹은 예금주명 : ");
+				String search = scan.next();
+				oos.writeUTF(search);
+				oos.flush();
+				int index = ois.readInt();
+				if(index == -1) {
+					System.out.println("없는 계좌입니다.");
+					break;
+				}
+				String check = "";
+				do {
+					System.out.print("비밀번호 : ");
+					String password = scan.next();
+					oos.writeUTF(password);
+					oos.flush();
+					check = ois.readUTF();
+					if(check.equals("잘못된 비밀번호입니다. 다시 입력하세요.")) {
+						System.out.println(check);
+						continue;
+					}
+					break;
+				}while(true);
+				String result = ois.readUTF();
+				if(result.equals("잔액이 0원이므로 출금할 수 없습니다.")) {
+					System.out.println(result);
+				}
+				else {
+					int withdraw = 0;
+					result = ois.readUTF();
+					System.out.print(result);
+					withdraw = scan.nextInt();
+					oos.writeInt(withdraw);
+					oos.flush();
+					result = ois.readUTF();
+					if(result.equals(withdraw+"원은 출금할 수 없습니다.")){
+						System.out.println(result);
 						break;
 					}
-					break;
-				case 5:
-					System.out.println("이전으로 돌아갑니다.");
-					System.out.println("------------------------------");
-					break;
-				default:
-					System.out.println("잘못된 메뉴입니다.");
+					System.out.println(withdraw+"원을 출금하였습니다.");
+					result = ois.readUTF();
+					System.out.println(result);
 					System.out.println("------------------------------");
 				}
-			}while(select != 5);
-			break;
-		case 3:
-			System.out.println("업무를 종료합니다.");
-			System.out.println("------------------------------");
+				break;
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		case 6:
+			try {
+				Socket socket = new Socket(ip, port);
+				ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+				ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+				oos.writeUTF("transfer");
+				oos.flush();
+				System.out.print("계좌번호 혹은 예금주명 : ");
+				String search = scan.next();
+				oos.writeUTF(search);
+				oos.flush();
+				int index = ois.readInt();
+				if(index == -1) {
+					System.out.println("없는 계좌입니다.");
+					break;
+				}
+				String check = "";
+				do {
+					System.out.print("비밀번호 : ");
+					String password = scan.next();
+					oos.writeUTF(password);
+					oos.flush();
+					check = ois.readUTF();
+					if(check.equals("잘못된 비밀번호입니다. 다시 입력하세요.")) {
+						System.out.println(check);
+						continue;
+					}
+					break;
+				}while(true);
+				String result = ois.readUTF();
+				if(result.equals("잔액이 0원이므로 출금할 수 없습니다.")) {
+					System.out.println(result);
+					break;
+				}
+				System.out.print("송금할 계좌번호 혹은 예금주명 : ");
+				search = scan.next();
+				oos.writeUTF(search);
+				oos.flush();
+				result = ois.readUTF();
+				if(result.equals("송금할 계좌가 없습니다.")) {
+					System.out.println(result);
+					break;
+				}
+				int size = ois.readInt();
+				for(int i = 0; i < size; i++) {
+					result = ois.readUTF();
+					System.out.println(result);
+				}
+				System.out.print("송금할 계좌 선택 : ");
+				int index2 = scan.nextInt() - 1;
+				oos.write(index2);
+				oos.flush();
+				
+				int transfer = 0;
+				result = ois.readUTF();
+				if(result.equals("번호를 잘못선택했습니다.")) {
+					System.out.println(result);
+					break;
+				}
+				System.out.print(result);
+				transfer = scan.nextInt();
+				oos.writeInt(transfer);
+				oos.flush();
+				result = ois.readUTF();
+				if(result.equals(transfer+"원은 송금할 수 없습니다.")) {
+					System.out.println(result);
+					break;
+				}
+				System.out.println(transfer+"원을 송금하였습니다.");
+				result = ois.readUTF();
+				System.out.println(result);
+				System.out.println("------------------------------");
+				break;
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		case 7:
+			try {
+				Socket socket = new Socket(ip, port);
+				ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+				ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+				oos.writeUTF("check");
+				oos.flush();
+				System.out.print("계좌번호 혹은 예금주명 : ");
+				String search = scan.next();
+				oos.writeUTF(search);
+				oos.flush();
+				int index = ois.readInt();
+				if(index == -1) {
+					System.out.println("없는 계좌입니다.");
+					break;
+				}
+				String check = "";
+				do {
+					System.out.print("비밀번호 : ");
+					String password = scan.next();
+					oos.writeUTF(password);
+					oos.flush();
+					check = ois.readUTF();
+					if(check.equals("잘못된 비밀번호입니다. 다시 입력하세요.")) {
+						System.out.println(check);
+						continue;
+					}
+					break;
+				}while(true);
+				String result = ois.readUTF();
+				System.out.println(result);
+				System.out.println("============통장내역============");
+				int size = ois.readInt();
+				for(int i = 0; i < size; i++) {
+					result = ois.readUTF();
+					System.out.println(result);
+				}
+				System.out.println("==============================");
+				System.out.print("돌아가려면 엔터를 입력하세요.");
+				scan.nextLine();
+				String enter = scan.nextLine();
+				if(enter.equals("\n")) {
+					break;
+				}
+				break;
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		case 8:
+			try {
+				Socket socket = new Socket(ip, port);
+				ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+				ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+				oos.writeUTF("end");
+				oos.flush();
+				String result = ois.readUTF();
+				System.out.println(result);
+				System.out.println("------------------------------");
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 			break;
 		default:
-			System.out.println("잘못된 메뉴입니다.");
-			System.out.println("------------------------------");
-		}
-	}
-	private int findAccount() {
-		System.out.print("계좌번호 혹은 예금주명 : ");
-		String search = scan.next();
-		int index = -1;
-		for(int i = 0; i < list.size(); i++) {
-			if(list.get(i).getAccountNum().equals(search) || list.get(i).getName().equals(search)) {
-				index = i;
+			try {
+				Socket socket = new Socket(ip, port);
+				ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+				ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+				oos.writeUTF("");
+				oos.flush();
+				System.out.println("잘못된 메뉴입니다.");
+				System.out.println("------------------------------");
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 		}
-		if(index == -1) {
-			System.out.println("없는 계좌입니다.");
-			return index;
-		}
-		return index;
 	}
-	
-	private void checkPassWord(int index) {
-		do {
-			System.out.print("비밀번호 : ");
-			String password = scan.next();
-			if(!list.get(index).getPassword().equals(password)) {
-				System.out.println("잘못된 비밀번호입니다. 다시 입력하세요.");
-				continue;
-			}
-			break;
-		}while(true);
-	}
-	
 	public void run() {
 		int menu = 0;
 		do {
 			printMenu();
 			menu = scan.nextInt();
 			runMenu(menu);
-		}while(menu != 3);
+		}while(menu != 8);
 	}
 }
--- project1/src/atm2/Server.java
+++ project1/src/atm2/Server.java
@@ -27,7 +27,7 @@ public class Server {
 				ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
 				ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
 				String type = ois.readUTF();
-				switch(type) {
+				A:switch(type) {
 				case "insert":
 					int result = 0;
 					String accountNum = "";
@@ -39,7 +39,6 @@ public class Server {
 								result++;
 							}
 						}
-						System.out.println(result);
 					}while(result != 0);
 					oos.writeUTF(accountNum);
 					oos.flush();
@@ -52,6 +51,224 @@ public class Server {
 						e.printStackTrace();
 					}
 					break;
+				case "remove":
+					String search = ois.readUTF();
+					int index = findAccount(search);
+					oos.writeInt(index);
+					oos.flush();
+					do {
+						String password = ois.readUTF();
+						if(!list.get(index).getPassword().equals(password)) {
+							oos.writeUTF("잘못된 비밀번호입니다. 다시 입력하세요.");
+							oos.flush();
+							continue;
+						}
+						oos.writeUTF("");
+						oos.flush();
+						break;
+					}while(true);
+					list.remove(index);
+					break;
+				case "update":
+					search = ois.readUTF();
+					index = findAccount(search);
+					oos.writeInt(index);
+					oos.flush();
+					do {
+						String password = ois.readUTF();
+						if(!list.get(index).getPassword().equals(password)) {
+							oos.writeUTF("잘못된 비밀번호입니다. 다시 입력하세요.");
+							oos.flush();
+							continue;
+						}
+						oos.writeUTF("");
+						oos.flush();
+						break;
+					}while(true);
+					String password = ois.readUTF();
+					list.get(index).setPassword(password);
+					break;
+				case "deposit":
+					int deposit = ois.readInt();
+					if(deposit <= 0) {
+						oos.writeUTF(deposit+"원은 입금할 수 없습니다.");
+						oos.flush();
+						break;
+					}
+					oos.writeUTF("");
+					oos.flush();
+					search = ois.readUTF();
+					index = findAccount(search);
+					oos.writeInt(index);
+					oos.flush();
+					if(index == -1) {
+						break;
+					}
+					int money = 0;
+					money = list.get(index).getBalance() + deposit;
+					list.get(index).setBalance(money);
+					oos.writeUTF(list.get(index).getName()+"님의 남은 잔고 : "+list.get(index).getBalance()+"원");
+					oos.flush();
+					list.get(index).getBankBook().add("입금 "+deposit+"원 | 잔고 : "+list.get(index).getBalance()+"원");
+					break;
+				case "withdraw":
+					search = ois.readUTF();
+					index = findAccount(search);
+					oos.writeInt(index);
+					oos.flush();
+					if(index == -1) {
+						break;
+					}
+					do {
+						password = ois.readUTF();
+						if(!list.get(index).getPassword().equals(password)) {
+							oos.writeUTF("잘못된 비밀번호입니다. 다시 입력하세요.");
+							oos.flush();
+							continue;
+						}
+						oos.writeUTF("");
+						oos.flush();
+						break;
+					}while(true);
+					if(list.get(index).getBalance() == 0) {
+						oos.writeUTF("잔액이 0원이므로 출금할 수 없습니다.");
+						oos.flush();
+					}
+					else {
+						oos.writeUTF("");
+						oos.flush();
+						int withdraw = 0;
+						oos.writeUTF("출금할 금액(잔액 : "+list.get(index).getBalance()+"원) : ");
+						oos.flush();
+						withdraw = ois.readInt();
+						if(withdraw <= 0 || list.get(index).getBalance() < withdraw) {
+							oos.writeUTF(withdraw+"원은 출금할 수 없습니다.");
+							oos.flush();
+							break;
+						}
+						oos.writeUTF("");
+						oos.flush();
+						money = list.get(index).getBalance() - withdraw;
+						list.get(index).setBalance(money);
+						oos.writeUTF(list.get(index).getName()+"님의 남은 잔고 : "+list.get(index).getBalance()+"원");
+						oos.flush();
+						list.get(index).getBankBook().add("출금 "+withdraw+"원 | 잔고 : "+list.get(index).getBalance()+"원");
+					}
+					break;
+				case "transfer":
+					search = ois.readUTF();
+					index = findAccount(search);
+					oos.writeInt(index);
+					oos.flush();
+					if(index == -1) {
+						break;
+					}
+					do {
+						password = ois.readUTF();
+						if(!list.get(index).getPassword().equals(password)) {
+							oos.writeUTF("잘못된 비밀번호입니다. 다시 입력하세요.");
+							oos.flush();
+							continue;
+						}
+						oos.writeUTF("");
+						oos.flush();
+						break;
+					}while(true);
+					if(list.get(index).getBalance() == 0) {
+						oos.writeUTF("잔액이 0원이므로 출금할 수 없습니다.");
+						oos.flush();
+					}
+					else {
+						oos.writeUTF("");
+						oos.flush();
+						search = ois.readUTF();
+						List<Account> list2 = new ArrayList<Account>();
+						for(int i = 0; i < list.size(); i++) {
+							if(index == i) {
+								continue;
+							}
+							if(list.get(i).getAccountNum().contains(search) || list.get(i).getName().contains(search)) {
+								list2.add(list.get(i));
+							}
+						}
+						if(list2.size() == 0) {
+							oos.writeUTF("송금할 계좌가 없습니다.");
+							oos.flush();
+							break;
+						}
+						oos.writeUTF("");
+						oos.writeInt(list2.size());
+						oos.flush();
+						for(int i = 0; i < list2.size(); i++) {
+							oos.writeUTF((i+1)+". "+list2.get(i).getBank()+" "+list2.get(i).getAccountNum()+"(예금주:"+list2.get(i).getName()+")");
+							oos.flush();
+						}
+						int index2 = ois.read();
+						if(index2 > list2.size() - 1 || index2 < 0) {
+							oos.writeUTF("번호를 잘못선택했습니다.");
+							oos.flush();
+							break;
+						}
+						int transfer = 0;
+						oos.writeUTF("송금할 금액(잔액 : "+list.get(index).getBalance()+"원) : ");
+						oos.flush();
+						transfer = ois.readInt();
+						if(transfer <= 0 || list.get(index).getBalance() < transfer) {
+							oos.writeUTF(transfer+"원은 송금할 수 없습니다.");
+							oos.flush();
+							break;
+						}
+						oos.writeUTF("");
+						oos.flush();
+						money = list.get(index).getBalance() - transfer;
+						list.get(index).setBalance(money);
+						oos.writeUTF(list.get(index).getName()+"님의 남은 잔고 : "+list.get(index).getBalance()+"원");
+						oos.flush();
+						list.get(index).getBankBook().add("송금 "+transfer+"원 | 잔고 : "+list.get(index).getBalance()+"원");
+						for(int i = 0; i < list.size(); i++) {
+							if(list.get(i).getAccountNum().equals(list2.get(index2).getAccountNum())) {
+								index2 = i;
+							}
+						}
+						money = list.get(index2).getBalance() + transfer;
+						list.get(index2).setBalance(money);
+						list.get(index2).getBankBook().add(list.get(index).getName()+" "+transfer+"원 | 잔고 : "+list.get(index2).getBalance()+"원");
+					}
+					break;
+				case "check":	
+					search = ois.readUTF();
+					index = findAccount(search);
+					oos.writeInt(index);
+					oos.flush();
+					if(index == -1) {
+						break;
+					}
+					do {
+						password = ois.readUTF();
+						if(!list.get(index).getPassword().equals(password)) {
+							oos.writeUTF("잘못된 비밀번호입니다. 다시 입력하세요.");
+							oos.flush();
+							continue;
+						}
+						oos.writeUTF("");
+						oos.flush();
+						break;
+					}while(true);
+					oos.writeUTF(list.get(index).toString());
+					oos.flush();
+					List<String> bankBook = list.get(index).getBankBook();
+					oos.writeInt(bankBook.size());
+					oos.flush();
+					for(int i = 0; i < bankBook.size(); i++) {
+						oos.writeUTF((i+1)+". "+bankBook.get(i));
+						oos.flush();
+					}
+					break;
+				case "end":
+					oos.writeUTF("업무를 종료합니다.");
+					oos.flush();
+					break;
+				default:
 				}
 			}
 		} catch (IOException e) {
@@ -59,6 +276,15 @@ public class Server {
 			e.printStackTrace();
 		}
 		save(fileName);
+	}
+	private static int findAccount(String search) {
+		int index = -1;
+		for(int i = 0; i < list.size(); i++) {
+			if(list.get(i).getAccountNum().equals(search) || list.get(i).getName().equals(search)) {
+				index = i;
+			}
+		}
+		return index;
 	}
 	private static String createAccountNum() {
 		int min = 1, max = 9999;
